675229a5ab510a4a5d9e41f4705d4f10feebb2c7
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-05-07 13:22:17 +0200

    Escape single quotes

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,7 +30,7 @@ const Home: NextPage = () => {
       `}
     >
       <Head>
-        <title>FADA - Film Autour De L'art</title>
+        <title>FADA - Film Autour De L&apos;art</title>
         <meta
           name="description"
           content="Festival organisé à Châteauvert dans le Var"
@@ -75,15 +75,15 @@ const Home: NextPage = () => {
               font-style: italic;
             `}
           >
-            Le film sur l'art élargit ses perspectives, raconte les artistes,
-            leurs démarches, leurs oeuvres. <br />
+            Le film sur l&apos;art élargit ses perspectives, raconte les
+            artistes, leurs démarches, leurs oeuvres. <br />
             Avec une programmation abordant musique, arts plastiques, danse,
-            littérature, poésie, historie de l'art et photographie, sous
+            littérature, poésie, historie de l&apos;art et photographie, sous
             différents formats et formes cinématographiques le FADA se prpose
-            d'accroìtre la connaissance et l'appréciation de l'art, mais aussi
-            de faire reconnaître le travail des artistes, ainsi que l'apport des
-            professionnel/le/s œuvrant dans les domaines du cinéma, de la vidéo
-            et de la télévision.
+            d&apos;accroìtre la connaissance et l&apos;appréciation de
+            l&apos;art, mais aussi de faire reconnaître le travail des artistes,
+            ainsi que l&apos;apport des professionnel/le/s œuvrant dans les
+            domaines du cinéma, de la vidéo et de la télévision.
           </blockquote>
           <figcaption>
             — Béatrice Pellegrino, co-foundatrice du festival, en 2014.
@@ -118,7 +118,7 @@ const Title = () => (
         flex-direction: column;
       `}
     >
-      <span>FILM AUTOUR DE L'ART. </span>
+      <span>FILM AUTOUR DE L&apos;ART. </span>
       <span>
         CHÀTEAUVERT. <b>VAR.</b>
       </span>
