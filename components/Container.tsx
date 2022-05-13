d2224ef5a890486eaf269faf97cde0d9751b89c3
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-05-13 17:11:21 +0200

    Increase padding on high resolutions

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,18 +1,25 @@
-import { css } from "@emotion/react";
+import { css, useTheme } from "@emotion/react";
 
 interface Props {
   children: React.ReactNode;
 }
-const Container: React.FC<Props> = ({ children }) => (
-  <div
-    css={css`
-      padding: 1rem;
-      margin: 0 auto;
-      max-width: 960px;
-    `}
-  >
-    {children}
-  </div>
-);
+const Container: React.FC<Props> = ({ children }) => {
+  const theme = useTheme();
+
+  return (
+    <div
+      css={css`
+        padding: 1rem;
+        margin: 0 auto;
+        max-width: 960px;
+        ${theme.mq.sm} {
+          padding: 0 4rem;
+        }
+      `}
+    >
+      {children}
+    </div>
+  );
+};
 
 export default Container;
