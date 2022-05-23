d7196239b35adc273d0908d8892b5731ce066fbd
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-05-23 15:34:55 +0200

    Create types for program display

---
+++ utils/types.ts
@@ -0,0 +1,23 @@
+interface DayEvent {
+  time: string;
+}
+
+export interface Show extends DayEvent {
+  title: string;
+  author: string;
+  // Year of release
+  year: number;
+  // Duration in minutes
+  duration: number;
+  // Additional information
+  notes?: string;
+}
+
+export interface Break extends DayEvent {
+  name: "Dîner";
+}
+
+export interface Day {
+  date: string;
+  events: Array<Show | Break>;
+}
