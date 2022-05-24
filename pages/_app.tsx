a9a2673b52b384eb4c4deb48b5037d1025cf9665
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-05-24 16:39:27 +0200

    Style h1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,19 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { ThemeProvider } from "@emotion/react";
+import { css, Global, ThemeProvider } from "@emotion/react";
 import { theme } from "../styles/theme";
 import Navbar from "components/Navbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+      <Global
+        styles={{
+          h1: {
+            color: theme.color.primary,
+          },
+        }}
+      />
       <Navbar />
       <Component {...pageProps} />
     </ThemeProvider>
