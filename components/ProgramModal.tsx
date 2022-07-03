e677a3f990334c87a90f34a4757ffb5533b2ad9b
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-07-03 19:24:43 +0200

    Handle scroll when modal is open

--- components/ProgramModal.tsx
+++ components/ProgramModal.tsx
@@ -1,43 +1,53 @@
 import { css } from "@emotion/react";
 import { ImageDTO } from "models/models.image";
 import Image from "next/image";
+import { useEffect } from "react";
 import ClientOnlyPortal from "./ClientOnlyPortal";
 
 interface ProgramModalProps {
   program: ImageDTO | null;
   handleClose: () => void;
 }
-const ProgramModal: React.FC<ProgramModalProps> = (props) => (
-  <ClientOnlyPortal>
-    {props.program && (
-      <div
-        css={css`
-          position: absolute;
-          width: 100%;
-          height: 100%;
-          background: rgba(0, 0, 0, 0.5);
-          z-index: 2;
-          overflow-y: scroll;
-        `}
-        onClick={props.handleClose}
-      >
+const ProgramModal: React.FC<ProgramModalProps> = (props) => {
+  useEffect(() => {
+    document.body.style.overflow = props.program ? "hidden" : "auto";
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [props.program]);
+
+  return (
+    <ClientOnlyPortal>
+      {props.program && (
         <div
           css={css`
-            width: 60%;
-            margin: auto;
+            position: fixed;
+            width: 100%;
+            height: 100%;
+            background: rgba(0, 0, 0, 0.7);
+            z-index: 2;
+            overflow-y: scroll;
           `}
         >
-          <Image
-            src={props.program.src}
-            alt={props.program.alt}
-            layout="responsive"
-            objectFit="contain"
-            placeholder="blur"
-          />
+          <div
+            css={css`
+              width: 60%;
+              margin: auto;
+            `}
+          >
+            <Image
+              src={props.program.src}
+              alt={props.program.alt}
+              layout="responsive"
+              objectFit="contain"
+              placeholder="blur"
+            />
+          </div>
         </div>
-      </div>
-    )}
-  </ClientOnlyPortal>
-);
+      )}
+    </ClientOnlyPortal>
+  );
+};
 
 export default ProgramModal;
